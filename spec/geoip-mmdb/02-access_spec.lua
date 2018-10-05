271480ca00d64afc7d9cb81559ca4abfa1040fa8
Michael Pollett <michael.pollett@datacamp.com>
2018-10-05 12:59:45 +0100

    test 10 address

--- spec/geoip-mmdb/02-access_spec.lua
+++ spec/geoip-mmdb/02-access_spec.lua
@@ -113,13 +113,24 @@ describe("Plugin: geoip-mmdb (access)", function()
   end)
 
   describe("invalid ip", function()
-    it("should be allowed", function()
+    it("local should be allowed", function()
       local res = assert(client:send {
         method  = "GET",
         path    = "/status/200",
         headers = {
           ["Host"] = "test1.com",
           ["X-Forwarded-For"] = "127.0.0.1"
+        }
+      })
+      assert.res_status(200, res)
+    end)
+    it("network should be allowed" function()
+      local res = assert(client:send {
+        method  = "GET",
+        path    = "/status/200",
+        headers = {
+          ["Host"] = "test1.com",
+          ["X-Forwarded-For"] = "10.1.1.1"
         }
       })
       assert.res_status(200, res)
